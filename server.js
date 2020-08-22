12ab6c025350245ae71db27aec64d2af9599f70d
Arjun C R <42132166+AJaxRUN@users.noreply.github.com>
2020-08-22 18:46:40 +0530

    final loosu

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000 || process.env.PORT;
 
 app.get("/", (req, res) => {
+    console.log("Received Request");
     console.log(req.query.cookie);
+    res.send("Heyy");
 });
 
 
